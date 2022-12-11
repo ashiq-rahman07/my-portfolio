f449556adde100ea67884f88307df4aa25dca2dc
ashiq77 <web3.0.ashiq@gmail.com>
2022-12-11 15:41:06 +0600

    project section add

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,27 +12,33 @@ const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
+      name: 'Education Corner',
       src: reactApp,
       path:'https://education-corner-quiz.netlify.app/'
     },
     {
       id: 2,
+      name: 'Tech Academy',
       src: techAcademy,
     },
     {
       id: 3,
+      name: 'Tv House',
       src: tvHouse,
     },
     {
       id: 4,
+      name: 'Learners',
       src: learners,
     },
     {
       id: 5,
+      name: 'Dent Fitt',
       src: dentFit,
     },
     {
       id: 6,
+      name: 'Fitness ',
       src: fitness,
     },
   ];
@@ -40,7 +46,7 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+      className="bg-gradient-to-b from-black to-gray-800 w-full mb-50 text-white "
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -51,7 +57,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src,path }) => (
+          {portfolios.map(({ id, src,path,name }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 data-aos="flip-left"
@@ -61,13 +67,18 @@ const Portfolio = () => {
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
+             
+              <h2 data-aos="fade-down" data-aos-duration="2000" className=" py-1 rounded text-center bg-slate-800 w-2/3 mx-auto mt-4  duration-200 hover:scale-105" >{name}</h2>
+               
+         
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button className="  m-4 duration-200 hover:scale-105">
                 <a href={path}>Live Site</a>
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button className="  m-4 duration-200 hover:scale-105">
                   Code
                 </button>
+                <a href="">Project Details</a>
               </div>
             </div>
           ))}
